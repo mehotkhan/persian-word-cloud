7e165bfd9710395c6db821ba35f6b47f6f4878b7
Javad Bahoosh <jb198473@gmail.com>
2017-07-06 02:56:38 +0430

    add examples to project and sample output to README.md

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 # how to install :
 * ```pip install persian_wordcloud```
 * see example folder
-* fork from  <a href="https://github.com/amueller/word_cloud">word_cloud/</a>
+* fork from  <a href="https://github.com/amueller/word_cloud">word_cloud</a>
 * see Example folder
+* tested on python 3
+# Sample Output:
+![Sample Result](example/result.png?raw=true)
--- example/simple.py
+++ example/simple.py
@@ -1,36 +1,36 @@
-# #!/usr/bin/env python
-# """
-# Minimal Example
-# ===============
-#
-# Generating a square wordcloud from the US constitution using default arguments.
-# """
-# # from os import path
-# # from persian_wordcloud.persian_wordcloud import STOPWORDS, PersianWordCloud
-# from persian_wordcloud import
-#
-#
-# d = path.dirname(__file__)
-#
-# text = open(path.join(d, 'persian.txt'), encoding='utf-8').read()
-#
-# # Generate a word cloud image
-#
-# stopwords = set(STOPWORDS)
-# # print(stopwords)
-# # exit()
-# wordcloud = PersianWordCloud(
-#     max_words=100,
-#     stopwords=stopwords,
-#     # mask=mask,
-#     margin=0,
-#     width=800,
-#     height=800,
-#     min_font_size=1,
-#     max_font_size=500,
-#     background_color="white"
-#     # random_state=1
-# ).generate(text)
-#
-# image = wordcloud.to_image()
-# image.show()
+#!/usr/bin/env python
+"""
+Minimal Example
+===============
+
+Generating a square wordcloud from the US constitution using default arguments.
+"""
+from os import path
+from persian_wordcloud.wordcloud import STOPWORDS, PersianWordCloud
+
+
+d = path.dirname(__file__)
+
+text = open(path.join(d, 'persian.txt'), encoding='utf-8').read()
+
+# Add another stopword
+
+STOPWORDS.add('اینکه')
+stopwords = set(STOPWORDS)
+
+# Generate a word cloud image
+
+wordcloud = PersianWordCloud(
+    max_words=100,
+    stopwords=stopwords,
+    margin=0,
+    width=800,
+    height=800,
+    min_font_size=1,
+    max_font_size=500,
+    background_color="black"
+).generate(text)
+
+image = wordcloud.to_image()
+image.show()
+image.save('result.png')
