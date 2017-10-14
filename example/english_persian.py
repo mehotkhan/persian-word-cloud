0390bd426e64d698751f1e61670b290af4f6f363
hosein <hs.ramezanpoor@gmail.com>
2017-10-15 00:46:45 +0330

    Fix relative imports bug

--- example/english_persian.py
+++ example/english_persian.py
@@ -6,7 +6,7 @@ Minimal Example
 Generating a square wordcloud from the US constitution using default arguments.
 """
 from os import path
-from persian_wordcloud.wordcloud import PersianWordCloud, add_stop_words
+from persian_wordcloud import PersianWordCloud, add_stop_words
 from wordcloud import STOPWORDS as EN_STOPWORDS
 
 d = path.dirname(__file__)
---
+++ persian_wordcloud/__init__.py
@@ -0,0 +1,1 @@
+from wordcloud_handler import *
